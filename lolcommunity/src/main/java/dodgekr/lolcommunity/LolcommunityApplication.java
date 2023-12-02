5cebd167db4893a229e3d7a9520cf8620a3d1557
ksh03003 <74225015+kwakchaewon@users.noreply.github.com>
2023-12-02 10:49:11 +0900

    initial commit

---
+++ lolcommunity/src/main/java/dodgekr/lolcommunity/LolcommunityApplication.java
@@ -0,0 +1,13 @@
+package dodgekr.lolcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolcommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LolcommunityApplication.class, args);
+	}
+
+}
---
+++ lolcommunity/src/test/java/dodgekr/lolcommunity/LolcommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package dodgekr.lolcommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LolcommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
