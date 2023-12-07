c13f261047976ac850b8934a51d82a1428c5a8f1
ksh03003 <74225015+kwakchaewon@users.noreply.github.com>
2023-12-07 18:41:58 +0900

    게시글 전체 조회 및 페이지네이션 적용

--- lolcommunity/src/main/java/dodgekr/lolcommunity/board/BoardController.java
+++ lolcommunity/src/main/java/dodgekr/lolcommunity/board/BoardController.java
@@ -4,12 +4,15 @@ import dodgekr.lolcommunity.member.Member;
 import dodgekr.lolcommunity.member.MemberService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
 
@@ -45,14 +48,16 @@ public class BoardController {
         //2. 에러가 없다면 질문 등록
         Member member = memberService.getMember(principal.getName());
         this.boardService.create(boardForm.getTitle(), boardForm.getContent(), member);
-        return "redirect:/";
-        // return "redirect:/board/list";
+        return "redirect:/board/list";
     }
 
     /**
-     *  게시글 전체 조회
+     *  게시글 전체 조회 (페이지네이션 적용)
      */
-
-
-
+    @GetMapping("/list")
+    public String list(Model model, @RequestParam(value="page", defaultValue="1") int page) {
+        Page<Board> paging = this.boardService.getBoardList(page-1);
+        model.addAttribute("paging", paging);
+        return "board_list";
+    }
 }
--- lolcommunity/src/main/java/dodgekr/lolcommunity/board/BoardService.java
+++ lolcommunity/src/main/java/dodgekr/lolcommunity/board/BoardService.java
@@ -2,9 +2,15 @@ package dodgekr.lolcommunity.board;
 
 import dodgekr.lolcommunity.member.Member;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -15,8 +21,17 @@ public class BoardService {
         Board board = new Board();
         board.setTitle(title);
         board.setContent(content);
-        board.setCreated_at(LocalDateTime.now());
+        board.setCreatedAt(LocalDateTime.now());
         board.setAuthor(author);
         this.boardRepository.save(board);
+    }
+
+    public Page<Board> getBoardList(int page) {
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createdAt"));
+
+        //pagesize: 한 페이지당 게시글 수
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
+        return this.boardRepository.findAll(pageable);
     }
 }
