aaf12a4a8fc78f9b9165b4c01e8f0b35c0446637
ksh03003 <74225015+kwakchaewon@users.noreply.github.com>
2023-12-07 14:57:13 +0900

    post, comment 리포지토리 생성

---
+++ lolcommunity/src/main/java/dodgekr/lolcommunity/comment/CommentRepository.java
@@ -0,0 +1,6 @@
+package dodgekr.lolcommunity.comment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Integer> {
+}
