a2bc98ba6cfd12c226cb0884150f928427520903
ksh03003 <74225015+kwakchaewon@users.noreply.github.com>
2023-12-22 01:16:18 +0900

    Update BoardRepository.java

--- lolcommunity/src/main/java/dodgekr/lolcommunity/board/BoardRepository.java
+++ lolcommunity/src/main/java/dodgekr/lolcommunity/board/BoardRepository.java
@@ -11,7 +11,6 @@ import java.util.List;
 public interface BoardRepository extends JpaRepository<Board,Integer> {
     Page<Board> findAll(Pageable pageable);
     Page<Board> findAll(Specification<Board> spec, Pageable pageable);
-
 }
 
 
