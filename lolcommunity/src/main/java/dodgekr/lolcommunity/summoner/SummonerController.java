1ca46c6a92d7ec2e9b77f150fd7218326cb5f4a6
ksh03003 <74225015+kwakchaewon@users.noreply.github.com>
2023-12-27 19:21:12 +0900

    Update SummonerController.java

--- lolcommunity/src/main/java/dodgekr/lolcommunity/summoner/SummonerController.java
+++ lolcommunity/src/main/java/dodgekr/lolcommunity/summoner/SummonerController.java
@@ -14,36 +14,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class SummonerController {
 
     private final SummonerService summonerService;
-//    @RequestMapping(value = "/summoner", method = RequestMethod.GET)
-//    public String searchSummoner(Model model, @PathVariable String summonerName){
-////                                 @RequestParam(value = "summoner", defaultValue = "너에게난뭐냐") String summonerName)
-//        BufferedReader br = null;
-////        Summoner temp= null;
-//
-//        try {
-//            String urlstr = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/"+ summonerName	+"?api_key=" + RIOT_API_KEY;
-//            URL url = new URL(urlstr);
-//
-//            HttpURLConnection urlconnection = (HttpURLConnection) url.openConnection();
-//            urlconnection.setRequestMethod("GET");
-//            br = new BufferedReader(new InputStreamReader(urlconnection.getInputStream(),"UTF-8")); // 여기에 문자열을 받아와라.
-//
-//            String result = "";
-//            String line;
-//
-//            while((line = br.readLine()) != null) {
-//                result = result + line;
-////                System.out.print(line);
-//            }
-//
-//            System.out.println(result);
-//
-//        }catch (Exception e){
-//            System.out.println(e.getMessage());
-//        }
-//
-//        return "summoner_detail";
-//    }
 
     @RequestMapping(value = "/summoner/{summonerName}", method = RequestMethod.GET)
     public String searchSummoner(Model model, @PathVariable String summonerName){
