875a94f3ebc7ff9ad973804afdc219b776383773
ksh03003 <74225015+kwakchaewon@users.noreply.github.com>
2024-01-02 18:20:39 +0900

    게임 정보 로직 디렉토리 변경
    컨트롤러 -> 서비스

--- lolcommunity/src/main/java/dodgekr/lolcommunity/summoner/SummonerController.java
+++ lolcommunity/src/main/java/dodgekr/lolcommunity/summoner/SummonerController.java
@@ -20,35 +20,16 @@ public class SummonerController {
 
     @RequestMapping(value = "/summoner/{summonerName}", method = RequestMethod.GET)
     public String searchSummoner(Model model, @PathVariable String summonerName){
-
         SummonerDTO summonerDTO = summonerService.findSummoner(summonerName);
         LeagueEntryDTO[] leagueEntryDTO = summonerService.findLeagueEntry(summonerDTO.getId());
         String[] matchList = summonerService.findMatchList(summonerDTO.getPuuid());
         MatchDto matchDto = summonerService.findMatch(matchList[0]);
+        MatchDto.ParticipantDto playerRecord= summonerService.getPlayerRecord(summonerDTO.getPuuid(), matchDto, summonerDTO);
 
         model.addAttribute("summonerInfo", summonerDTO);
         model.addAttribute("profileUrl", "http://ddragon.leagueoflegends.com/cdn/13.24.1/img/profileicon/"+ summonerDTO.getProfileIconId() + ".png");
         model.addAttribute("entryInfo", leagueEntryDTO[0]);
-//        model.addAttribute("matchList", matchList);
-
-        MatchDto.ParticipantDto desirePlayer = new MatchDto.ParticipantDto();
-
-        List<MatchDto.ParticipantDto> participants = matchDto.getInfo().getParticipants();
-        // puuid 값을 비교해 본인의 participants 값만 가져오기
-        for(MatchDto.ParticipantDto p: participants){
-            if(p.getPuuid().equals(summonerDTO.getPuuid())){
-                desirePlayer.setPuuid(p.getPuuid());
-                desirePlayer.setTeamId(p.getTeamId());
-                desirePlayer.setWin(p.isWin());
-                desirePlayer.setKills(p.getKills());
-                desirePlayer.setDeaths(p.getDeaths());
-                desirePlayer.setAssists(p.getAssists());
-                desirePlayer.setChampionId(p.getChampionId());
-                desirePlayer.setChampionName(p.getChampionName());
-            }
-        }
-
-        model.addAttribute("desirePlayer", desirePlayer);
+        model.addAttribute("playerRecord", playerRecord);
         return "summoner_detail";
     }
 
--- lolcommunity/src/main/java/dodgekr/lolcommunity/summoner/SummonerService.java
+++ lolcommunity/src/main/java/dodgekr/lolcommunity/summoner/SummonerService.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 /**
  * 서비스 네이밍 원칙
  * 함수명:
@@ -36,5 +38,17 @@ public class SummonerService {
     @Transactional(readOnly = true)
     public MatchDto findMatch(String matchId){
         return riotApi.getMatch(matchId);
+    }
+
+    public MatchDto.ParticipantDto getPlayerRecord(String puuid, MatchDto matchDto, SummonerDTO summonerDTO){
+        MatchDto.ParticipantDto playerRecord = new MatchDto.ParticipantDto();
+        List<MatchDto.ParticipantDto> participants = matchDto.getInfo().getParticipants();
+
+        for(MatchDto.ParticipantDto p: participants){
+            if(p.getPuuid().equals(summonerDTO.getPuuid())){
+                playerRecord = p;
+            }
+        }
+        return playerRecord;
     }
 }
